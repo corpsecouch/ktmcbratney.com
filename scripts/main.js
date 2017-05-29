bc459afe284f4b8bd1471147f3ea627ee849e990
corpsecouch <jasonbejot@gmail.com>
2017-05-28 19:07:40 -0700

    removed email

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,3 @@
 $(function() {
-  var addr = ['ktmcbratney','+','letstalk','@','gmail','.','com'];
-  var sub = 'Let\'s Talk';
-  $('#email').click(function(evt){
-    evt.preventDefault();
-    window.location = 'mailto:' + addr.join('') + '?' + sub;
-  });
+
 });
